95f7209132f2cd68955136768d416ecc2e74ea19
Demiralp <demiralpali@gmail.com>
2017-07-03 23:40:12 +0200

    Implemented texture views.

--- include/gl/texture_view.hpp
+++ include/gl/texture_view.hpp
@@ -2,3 +2,55 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef GL_TEXTURE_VIEW_HPP_
+#define GL_TEXTURE_VIEW_HPP_
+
+#include <gl/opengl.hpp>
+#include <gl/texture.hpp>
+
+#include <export.hpp>
+
+namespace gl
+{
+template<GLenum target>
+class GL_EXPORT texture_view : public texture<target>
+{
+public:
+  // 8.18 Texture views.
+  texture_view(const texture<target>& original, GLenum internal_format, GLuint min_level, GLuint num_levels, GLuint min_layer, GLuint num_layers) : texture<target>()
+  {
+    glTextureView(texture<target>::id_, target, original.id(), internal_format, min_level, num_levels, min_layer, num_layers);
+  }
+  texture_view(const texture_view&  that) = delete ;
+  texture_view(      texture_view&& temp) = default;
+  virtual ~texture_view()                 = default;
+
+  texture_view& operator=(const texture_view&  that) = delete ;
+  texture_view& operator=(      texture_view&& temp) = default;
+
+  // 8.11 Texture queries.
+  GLint   min_level       () const
+  {
+    return texture<target>::get_int_parameter(GL_TEXTURE_VIEW_MIN_LEVEL);
+  }
+  GLsizei num_levels      () const
+  {
+    return texture<target>::get_int_parameter(GL_TEXTURE_VIEW_NUM_LEVELS);
+  }
+  GLint   min_layer       () const
+  {
+    return texture<target>::get_int_parameter(GL_TEXTURE_VIEW_MIN_LAYER);
+  }
+  GLsizei num_layers      () const
+  {
+    return texture<target>::get_int_parameter(GL_TEXTURE_VIEW_NUM_LAYERS);
+  }
+  GLsizei immutable_levels() const
+  {
+    return texture<target>::get_int_parameter(GL_TEXTURE_IMMUTABLE_LEVELS);
+  }
+};
+}
+
+#endif
--- source/texture_view.cpp
+++ source/texture_view.cpp
@@ -2,3 +2,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <gl/texture_view.hpp>
+
+namespace gl
+{
+
+}
